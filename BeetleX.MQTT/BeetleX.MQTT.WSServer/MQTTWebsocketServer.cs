bb7c320fc171611ad2c92a332eb4b25f45a7fbb6
DESKTOP-2N3GSM4\henry <henryfan@msn.com>
2022-11-17 10:48:29 +0800

    添加websocket服务

---
+++ BeetleX.MQTT/BeetleX.MQTT.WSServer/MQTTWebsocketServer.cs
@@ -0,0 +1,164 @@
+﻿using BeetleX.EventArgs;
+using BeetleX.FastHttpApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeetleX.MQTT.WSServer
+{
+    public class MQTTWebsocketServer<APPLICATION, SESSION> : FastHttpApi.Hosting.HttpServer
+        where APPLICATION : IApplication, new()
+        where SESSION : ISessionToken, new()
+    {
+        public MQTTWebsocketServer(int port, string host = null) : base(port, host)
+        {
+            MQTTApplication = new APPLICATION();
+        }
+
+        public APPLICATION MQTTApplication { get; set; }
+
+
+
+
+        protected override void OnCreating(HttpApiServer server)
+        {
+            base.OnCreating(server);
+        }
+        protected override void OnCompleted(HttpApiServer server)
+        {
+            MQTTApplication.Init(server.Server);
+            server.WebSocketReceive = (o, e) =>
+            {
+                if (e.Frame.Body != null)
+                {
+                    var items = (List<MQTTMessage>)e.Frame.Body;
+                    foreach (var item in items)
+                    {
+                        Type msgtype = item.GetType();
+                        if (mMessageHandlers.TryGetValue(msgtype, out IMessageProcessHandler handler))
+                        {
+                            handler.Execute(server, e.Request.Session, MQTTApplication, e.Request.Session.Token<SESSION>(), item);
+                        }
+                        else
+                        {
+                            OnReceiveProcess(server, e.Request.Session, item);
+
+                        }
+                    }
+                }
+            };
+            server.WebSocketConnect += (o, e) =>
+            {
+                e.Request.Session.Token<MQTTUser>();
+                MQTTUpgradeWebsocketSuccess upgradeWebsocket = new MQTTUpgradeWebsocketSuccess(e.Request.Header[HeaderTypeFactory.SEC_WEBSOCKET_KEY]);
+                e.UpgradeSuccess = upgradeWebsocket;
+            };
+            base.OnCompleted(server);
+        }
+
+        private System.Collections.Concurrent.ConcurrentDictionary<Type, IMessageProcessHandler> mMessageHandlers = new System.Collections.Concurrent.ConcurrentDictionary<Type, IMessageProcessHandler>();
+
+        public MQTTWebsocketServer<APPLICATION, SESSION> OnMessageReceive<Message>(Action<EventWSMessageReceiveArgs<APPLICATION, SESSION, Message>> handler)
+        {
+            MessageProcessHandler<APPLICATION, SESSION, Message> e = new MessageProcessHandler<APPLICATION, SESSION, Message>();
+            e.Handler = handler;
+            mMessageHandlers[typeof(Message)] = e;
+            return this;
+        }
+
+
+        private Action<EventWSMessageReceiveArgs<APPLICATION, SESSION, object>> mOnReceive;
+
+        protected virtual void OnReceiveProcess(HttpApiServer server, ISession session, object message)
+        {
+            EventWSMessageReceiveArgs<APPLICATION, SESSION, object> e = new EventWSMessageReceiveArgs<APPLICATION, SESSION, object>();
+            e.Application = MQTTApplication;
+            e.HttpApiServer = server;
+            e.NetSession = session;
+            e.Session = session.Token<SESSION>();
+            e.Message = message;
+            mOnReceive?.Invoke(e);
+        }
+
+        public MQTTWebsocketServer<APPLICATION, SESSION> OnMessageReceive(Action<EventWSMessageReceiveArgs<APPLICATION, SESSION, object>> handler)
+        {
+            mOnReceive = handler;
+            return this;
+        }
+
+
+    }
+    public class MQTTUpgradeWebsocketSuccess : ResultBase
+    {
+        public MQTTUpgradeWebsocketSuccess(string websocketKey)
+        {
+            WebsocketKey = websocketKey;
+        }
+
+        public string WebsocketKey { get; set; }
+
+
+        public override bool HasBody => false;
+
+        public override void Setting(HttpResponse response)
+        {
+            response.Code = "101";
+            response.CodeMsg = "Switching Protocols";
+            response.Header.Add(HeaderTypeFactory.CONNECTION, "Upgrade");
+            response.Header.Add(HeaderTypeFactory.UPGRADE, "websocket");
+            response.Header.Add(HeaderTypeFactory.SEC_WEBSOCKET_VERSION, "13");
+            SHA1 sha1 = new SHA1CryptoServiceProvider();
+            byte[] bytes_sha1_in = Encoding.UTF8.GetBytes(WebsocketKey + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11");
+            byte[] bytes_sha1_out = sha1.ComputeHash(bytes_sha1_in);
+            string str_sha1_out = Convert.ToBase64String(bytes_sha1_out);
+            response.Header.Add(HeaderTypeFactory.SEC_WEBSOCKT_ACCEPT, str_sha1_out);
+        }
+    }
+
+    public struct EventWSMessageReceiveArgs<APPLICATION, SESSION, MSG>
+         where SESSION : ISessionToken, new()
+   where APPLICATION : IApplication, new()
+    {
+        public ISession NetSession { get; set; }
+
+        public HttpApiServer HttpApiServer { get; set; }
+        public SESSION Session { get; set; }
+        public APPLICATION Application { get; set; }
+        public MSG Message { get; set; }
+
+        public ILoger GetLoger(LogType type)
+        {
+            return HttpApiServer.Server.GetLoger(type);
+        }
+        public void Return(MQTTMessage message)
+        {
+            var frame = HttpApiServer.CreateBinaryFrame(message);
+            NetSession.Send(frame);
+        }
+    }
+
+    class MessageProcessHandler<APPLICATION, SESSION, MESSAGE> : IMessageProcessHandler
+        where SESSION : ISessionToken, new()
+   where APPLICATION : IApplication, new()
+    {
+        public Action<EventWSMessageReceiveArgs<APPLICATION, SESSION, MESSAGE>> Handler { get; set; }
+        public void Execute(HttpApiServer server, object netsession, object application, object session, object message)
+        {
+            EventWSMessageReceiveArgs<APPLICATION, SESSION, MESSAGE> e = new EventWSMessageReceiveArgs<APPLICATION, SESSION, MESSAGE>();
+            e.NetSession = (ISession)netsession;
+            e.Application = (APPLICATION)application;
+            e.Session = (SESSION)session;
+            e.Message = (MESSAGE)message;
+            e.HttpApiServer = server;
+            Handler?.Invoke(e);
+        }
+    }
+
+    interface IMessageProcessHandler
+    {
+        void Execute(HttpApiServer server, object netsession, object application, object session, object message);
+    }
+}
---
+++ BeetleX.MQTT/BeetleX.MQTT.WSServer/Program.cs
@@ -0,0 +1,64 @@
+﻿using BeetleX.MQTT.Messages;
+using System;
+using System.Text;
+
+namespace BeetleX.MQTT.WSServer
+{
+    class Program
+    {
+        private static MQTTWebsocketServer<MQTTApplication, MQTTUser> mServer;
+        static void Main(string[] args)
+        {
+            mServer = new MQTTWebsocketServer<MQTTApplication, MQTTUser>(8081);
+            mServer.Setting((service, options) => {
+                options.LogLevel = EventArgs.LogType.Trace;
+                options.LogToConsole = true;
+                options.WebSocketFrameSerializer = new MQTTFormater();
+            });
+            mServer.OnMessageReceive<CONNECT>(e =>
+            {
+                e.GetLoger(EventArgs.LogType.Info)?.Log(EventArgs.LogType.Info, e.NetSession, $"{e.NetSession.RemoteEndPoint} connect name:{e.Message.UserName} password:{e.Message.Password}");
+                e.Session.UserName = e.Message.UserName;
+                e.Session.ID = e.Message.ClientID;
+                CONNACK ack = new CONNACK();
+                e.Return(ack);
+            })
+            .OnMessageReceive<SUBSCRIBE>(e =>
+            {
+                e.GetLoger(EventArgs.LogType.Info)?.Log(EventArgs.LogType.Info, e.NetSession, $"{e.Session.ID} subscribe {e.Message}");
+                SUBACK ack = new SUBACK();
+                ack.Identifier = e.Message.Identifier;
+                ack.Status = QoSType.MostOnce;
+                e.Return(ack);
+                e.Application.RegisterSubscribe(e.Message, e.Session);
+            })
+            .OnMessageReceive<UNSUBSCRIBE>(e =>
+            {
+                e.GetLoger(EventArgs.LogType.Info)?.Log(EventArgs.LogType.Info, e.NetSession, $"{e.Session.ID} unsubscribe {e.Message}");
+                UNSUBACK ack = new UNSUBACK();
+                e.Return(ack);
+                e.Application.UnRegisterSubscribe(e.Message, e.Session);
+            })
+            .OnMessageReceive<PUBLISH>(e =>
+            {
+                var data = Encoding.UTF8.GetString(e.Message.PayLoadData.Array, e.Message.PayLoadData.Offset, e.Message.PayLoadData.Count);
+                e.GetLoger(EventArgs.LogType.Info)?.Log(EventArgs.LogType.Info, e.NetSession, $"{e.Session.ID} publish {e.Message.Topic}@ {e.Message.Identifier} data:{data}");
+                PUBACK ack = new PUBACK();
+                ack.Identifier = e.Message.Identifier;
+                e.Return(ack);
+                e.Application.Publish(e.Message);
+            })
+            .OnMessageReceive<PINGREQ>(e =>
+            {
+                PINGRESP resp = new PINGRESP();
+                e.Return(resp);
+            })
+            .OnMessageReceive(e =>
+            {
+
+            })
+            .Run();
+            Console.Read();
+        }
+    }
+}
